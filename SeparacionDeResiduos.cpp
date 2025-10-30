3d33420bbee1820ea9d1ac3b423ccec495ec503f
IanUribe3131 <0284692@up.edu.mx>
2025-10-29 21:46:39 -0600

    figuring out how to do csv

--- SeparacionDeResiduos.cpp
+++ SeparacionDeResiduos.cpp
@@ -2,10 +2,20 @@
 #include <string>
 #include <vector>
 #include <sstream>
+#include <fstream>
 
 using namespace std;
 
 int main(){
 
-  
+    ifstream iFile;
+    string line="";
+    iFile.open("waste_items_dataset.csv");
+
+    getline(iFile, line);
+    getline(iFile, line);
+
+    cout<<line<<endl;
+
+    return 0;
 }
